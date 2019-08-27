f3a3eb6344b30369a1f9ebd1f8bc1299762d597d
Jerry <jerry092383@gmail.com>
2019-08-27 10:56:31 +0800

    update ver_code_check.php

--- verification/ver_code_check.php
+++ verification/ver_code_check.php
@@ -12,10 +12,8 @@ if (isset($_POST['verCode']) && isset($_POST['btn'])) {
         echo '驗證成功<br>';
         echo $_SESSION['verCode'];
         header('refresh: 5, url = ver_code_view.html');
-        session_destroy();
     } else {
         echo '驗證失敗';
-        session_destroy();
         header('refresh: 3, url = ver_code_view.html');
     }
 }
